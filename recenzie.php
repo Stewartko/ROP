51cfa4fd3581b447acb0d43fc525e93b27ce6834
dansolstudent <104197636+dansolstudent@users.noreply.github.com>
2022-12-07 20:48:38 +0100

    Add files via upload

---
+++ recenzie.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/header/headerS.css">
+    <link rel="stylesheet" href="styles/footer/footer.css">
+    <link rel="stylesheet" href="styles/recenzie/recenzie.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Aref+Ruqaa+Ink:wght@400;700&family=Montserrat:wght@400;500&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <div class="profile">
+            <a href="login.html" class="login">Log In</a>
+        </div>
+
+        <div class="logo">
+            <a href="index.html">
+                <img src="media/logo/gprotect-01.svg" alt="">
+            </a>
+        </div>
+
+        <input type="checkbox" id="click">
+        <label for="click" class="menu-btn">
+            <i class="fas fa-bars"></i>
+        </label>
+
+        <ul class="links">
+            <li class="link"><a href="#">Home</a></li>
+            <li class="link"><a href="">Sklá</a></li>
+            <li class="link"><a href="">Kryty</a></li>
+            <li class="link"><a href="" class="service">Kontakt</a></li>
+            <li class="link"><a href="" class="service">Reklamacie</a></li>
+            <li class="link"><a href="" class="profile">Profil</a></li>
+            <li class="link"><a href="recenzie.html" class="profile">Recenzie</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <div class="container">
+            <div class="inner">
+              <h1>Recenzie</h1>
+              <div class="border"></div>
+      
+              <div class="row">
+                <div class="col">
+                  <div class="review">
+                    <img src="/media/screen_protector.jpg" alt="">
+                    <div class="name">Full name</div>
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                    </div>
+      
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    </p>
+                  </div>
+                </div>
+      
+                <div class="col">
+                  <div class="review">
+                    <img src="/media/screen_protector.jpg" alt="">
+                    <div class="name">Full name</div>
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="far fa-star"></i>
+                      <i class="far fa-star"></i>
+                    </div>
+      
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    </p>
+                  </div>
+                </div>
+      
+                <div class="col">
+                  <div class="review">
+                    <img src="/media/screen_protector.jpg" alt="">
+                    <div class="name">Full name</div>
+                    <div class="stars">
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="fas fa-star"></i>
+                      <i class="far fa-star"></i>
+                    </div>
+      
+                    <p>
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    </p>
+                  </div>
+                </div>
+
+                <div class="col">
+                    <div class="review">
+                      <img src="/media/screen_protector.jpg" alt="">
+                      <div class="name">Full name</div>
+                      <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+        
+                      <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                      </p>
+                    </div>
+                  </div>
+                
+                  <div class="col">
+                    <div class="review">
+                      <img src="/media/screen_protector.jpg" alt="">
+                      <div class="name">Full name</div>
+                      <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+        
+                      <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                      </p>
+                    </div>
+                  </div>
+
+                  <div class="col">
+                    <div class="review">
+                      <img src="/media/screen_protector.jpg" alt="">
+                      <div class="name">Full name</div>
+                      <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+        
+                      <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                      </p>
+                    </div>
+                  </div>
+
+                  <div class="col">
+                    <div class="review">
+                      <img src="/media/screen_protector.jpg" alt="">
+                      <div class="name">Full name</div>
+                      <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+        
+                      <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                      </p>
+                    </div>
+                  </div>
+
+                  <div class="col">
+                    <div class="review">
+                      <img src="/media/screen_protector.jpg" alt="">
+                      <div class="name">Full name</div>
+                      <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+        
+                      <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                      </p>
+                    </div>
+                  </div>
+
+                  <div class="col">
+                    <div class="review">
+                      <img src="/media/screen_protector.jpg" alt="">
+                      <div class="name">Full name</div>
+                      <div class="stars">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                      </div>
+        
+                      <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                      </p>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+    </main>
+
+
+    <footer>
+        <div class="left">
+            <h3>Usefull links</h3>
+
+            <div class="links">
+                <a href="">O nás</a>
+                <a href="">Služby</a>
+                <a href="">Kontakt</a>
+                <a href="">Obchod</a>
+            </div>
+        </div>
+        <div class="center">
+            <h3>Social media</h3>
+
+            <div class="media">
+                <a href="" class="fa fa-instagram"></a>
+                <a href="" class="fa fa-facebook-square"></a>
+                <a href="" class="fa fa-twitter-square"></a>
+            </div>
+        </div>
+        <div class="right">
+            <h3>Something wrong?</h3>
+            <div class="links">
+                <a href="">Problem s prihlásením?</a>
+                <a href="">Nemáme tvoj produkt?</a>
+                <a href="">Reklamácie</a>
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
